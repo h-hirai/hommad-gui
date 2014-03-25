e0f90e070407da3ac61c8a7b1007019f6aa9bb9a
h-hirai <hirai.hajime@gmail.com>
2014-03-26 00:53:26 +0900

    viewing test board

--- Main.hs
+++ Main.hs
@@ -2,9 +2,7 @@ module Main where
 
 import qualified Graphics.Gloss as G
 import qualified HomMad as Hom
-
-backGroundColor :: G.Color
-backGroundColor = G.white
+import HomMad (Color (..))
 
 gridWidth :: Float
 gridWidth = 30
@@ -23,7 +21,49 @@ grid = G.pictures $
                 fromHomPoint (Hom.boardSize-1, col)]
         | col <- [0..Hom.boardSize-1]]
 
+board :: G.Picture
+board = G.rectangleSolid halfOfSize halfOfSize
+    where halfOfSize = gridWidth * fromIntegral Hom.boardSize
+
+showBoard :: Hom.Board -> G.Picture
+showBoard b = G.pictures
+              [showStone (Hom.boardRef b (row, col)) (row, col) |
+               row <- [0..Hom.boardSize-1], col <- [0..Hom.boardSize-1]]
+
+showStone :: Hom.Color -> Hom.Point -> G.Picture
+showStone E _ = G.blank
+showStone c p = G.color (fromHomColor c)
+                $ uncurry G.translate (fromHomPoint p)
+                $ G.circleSolid (gridWidth * 0.4)
+    where fromHomColor B = G.black
+          fromHomColor W = G.white
+
+backGroundColor :: G.Color
+backGroundColor = G.white
+
+boardColor :: G.Color
+boardColor = G.greyN 0.7
+
+gridColor :: G.Color
+gridColor = backGroundColor
+
+testBoard :: Hom.Board
+testBoard = [[E, B, E, E, E, E, E, E, E]
+            ,[B, E, E, E, E, B, E, E, E]
+            ,[E, E, E, E, E, E, E, E, E]
+            ,[E, E, E, E, E, B, E, E, E]
+            ,[E, E, E, E, E, E, E, E, E]
+            ,[E, E, E, W, E, W, E, E, E]
+            ,[E, E, E, E, E, W, E, E, E]
+            ,[E, E, E, E, E, E, E, E, E]
+            ,[E, E, E, E, E, E, E, E, E]
+            ]
+
 main :: IO ()
 main = G.display
        (G.InWindow "HomMad GUI" (500, 500) (50, 50))
-       backGroundColor grid
+       backGroundColor
+       $ G.pictures [G.color boardColor $ board
+                    ,G.color gridColor grid
+                    ,showBoard testBoard
+                    ]
